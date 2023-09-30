02f712b10dd6962e3489667573d47c6f889716d1
NganVo <nganvothikim270496@gmail.com>
2023-09-30 17:00:43 +0700

    Rename variable name in Lab2.3

--- src/lab_02_homework/Lab23.java
+++ src/lab_02_homework/Lab23.java
@@ -13,13 +13,13 @@ public class Lab23 {
          * 2. If BMI < 18.5 -> Underweight
          * 3. BMI = weight / (height x 2) -> If BMI increase -> weight increase
          *
-         * 4. Declare variable 'predictWeight' = 0.5 (kg)
+         * 4. Declare variable 'weightPlus' = 0.5 (kg)
          * 5. Declare variable 'currentWeight' = weight
          * 7. Calculate current BMI based on weight, height
-         * 8. If current BMI > 24.9 then weight decrease (weight + predictWeight * (-1))
-         * 9. If current BMI < 18.5 then weight increase (weight + predictWeight)
+         * 8. If current BMI > 24.9 then weight decrease (weight + weightPlus * (-1))
+         * 9. If current BMI < 18.5 then weight increase (weight + weightPlus)
          * 10. While (currentBMI < 18.5 && currentBMI > 24.9) then
-         *   10.1 weight = weight + predictWeight
+         *   10.1 weight = weight + weightPlus
          *   10.2 Re-calculate current BMI = weight / (height x 2)
          */
         Scanner scanner = new Scanner(System.in);
@@ -40,7 +40,7 @@ public class Lab23 {
             System.out.print("You mustn't change your weight");
         } else {
             double weightPlus = 0.5;
-            double weightCurrent = inputWeight;
+            double currentWeight = inputWeight;
             boolean isIncrease = true;
             if (calBMI > 24.9) {
                 weightPlus = weightPlus * (-1);
@@ -48,8 +48,8 @@ public class Lab23 {
             }
             while (calBMI < 18.5 || calBMI > 24.9) {
 
-                weightCurrent = weightCurrent + weightPlus;
-                calBMI = weightCurrent / (inputHeight * 2);
+                currentWeight = currentWeight + weightPlus;
+                calBMI = currentWeight / (inputHeight * 2);
 
                 if (calBMI >= 18.5 && calBMI <= 24.9) {
                     break;
@@ -58,10 +58,10 @@ public class Lab23 {
 
             double weightCal = 0;
             if (isIncrease) {
-                weightCal = weightCurrent - inputWeight;
+                weightCal = currentWeight - inputWeight;
                 System.out.println("You must increase your weight: " + weightCal + " (kg)");
             } else {
-                weightCal = inputWeight - weightCurrent;
+                weightCal = inputWeight - currentWeight;
                 System.out.println("You must decrease your weight: " + weightCal + " (kg)");
             }
         }
