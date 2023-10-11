42faf7111f982f5454240b9a29f4c0827e977db9
NganVo <nganvothikim270496@gmail.com>
2023-10-11 22:24:04 +0700

    Add new Lab6.1, Lab6.2, Lab6.3, Lab6.4

---
+++ src/lab_06_homework/Lab61.java
@@ -0,0 +1,31 @@
+package lab_06_homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Lab61 {
+    public static void main(String[] args) {
+        /*
+        * LAB 6.1
+        * Given input string: "2hrs and 5 minutes"
+        * Please calculate how many minutes in total
+        * */
+
+        String str = "2hrs and 5 minutes";
+        String[] strList = str.split(" ");
+        int totalMinute = 0;
+        int hours = 0;
+        int minute = 0;
+        for(int index = 0; index < strList.length; index++){
+            if(strList[index].contains("hrs")){
+                hours = Integer.parseInt(strList[index].replace("hrs", "")) ;
+            }
+            if(strList[index].contains("minutes")){
+                minute = Integer.parseInt(strList[index-1]) ;
+            }
+        }
+        totalMinute = hours * 60 + minute;
+        System.out.println("Minute total = " + totalMinute);
+    }
+}
---
+++ src/lab_06_homework/Lab64.java
@@ -0,0 +1,18 @@
+package lab_06_homework;
+
+public class Lab64 {
+    public static void main(String[] args) {
+        /* LAB 6.4
+        String url = "https://google.com";
+        Check http OR https; domain name, .com OR .net
+        */
+
+        String url = "https://google.com";
+        if (url.contains("https")) {
+            System.out.println("Url contains 'https'");
+        }
+        if (url.contains(".com")) {
+            System.out.println("Url contains '.com'");
+        }
+    }
+}
